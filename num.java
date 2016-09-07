226a5a3176479b2eb581f06d7380c22f5a7d86c3
KANI2511 <kanimozhidharshan@gmail.com>
2016-09-07 23:10:04 +0530

    Add files via upload

---
+++ num.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class num
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+int a=s.nextInt();
+if(a>0)
+System.out.println("positive integer");
+else if(a<0)
+System.out.println("negative integer");
+else
+System.out.println("zero");
+}
+}
