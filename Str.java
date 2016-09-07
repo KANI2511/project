c8de539463f2042d5e59f59a4d23c80172127af5
KANI2511 <kanimozhidharshan@gmail.com>
2016-09-07 12:24:37 +0530

    Add files via upload

---
+++ Str.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Str
+{
+public static void main(String[] args)
+{
+Scanner s=new Scanner(System.in);
+String a=s.next();
+StringBuffer b=new StringBuffer(a);
+System.out.println(b.reverse());
+}
+}
